d4d5fe7b54027f6d5eae55227e0a7b0ea174530a
Aaron Franks <aaron.franks@gmail.com>
2014-02-03 06:55:52 -0800

    Add <title>, <a>, and integer attrs for code graph.

--- js/homepage.js
+++ js/homepage.js
@@ -17,15 +17,19 @@ function timestamp(dateString) {
 var githubGraph = function(config) {
     var myRepos = config.data.filter(function(r) { return !r.fork })
                              .sort(function(r1, r2) { return timestamp(r1.created_at) - timestamp(r2.created_at); });
-    config.el.selectAll('circle').data(myRepos)
-             .enter().append('circle')
-             .attr('cx', function(d) {
-                 return (timestamp(d.pushed_at) - START_DATE)/(3600*1000*24);
-             })
-             .attr('cy', function(d, i) { return 30 + i*10 })
-             .attr('r', function(d) { return 10 + Math.sqrt(d.stargazers_count); })
-             .attr('title', function(d) { return d.name }) // FIXME: for debugging only
-             .attr('fill', 'rgba(20, 20, 190, 0.5)');      // TODO: color by language type
+    var all = config.el.selectAll('g.repo').data(myRepos);
+    var enter = all.enter().append('g').attr('class', 'repo');
+    enter.append('title')
+            .text(function(d) { return d.name });
+    var links = enter.append('a')
+            .attr('xlink:href', function(d) { return d.html_url });
+    links.append('circle')
+            .attr('cx', function(d) {
+                return Math.floor((timestamp(d.pushed_at) - START_DATE)/(3600*1000*24));
+            })
+            .attr('cy', function(d, i) { return 30 + i*10 })
+            .attr('r', function(d) { return 10 + Math.floor(Math.sqrt(d.stargazers_count)); })
+            .attr('class', function(d) { return d.language });
 };
 
 module.exports = function() {
