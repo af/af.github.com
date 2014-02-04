97c5faa2f9449cec882dbd0a0006a40c9988083a
Aaron Franks <aaron.franks@gmail.com>
2014-02-04 10:46:08 -0800

    Draw "comet" shapes for repos instead of plain circles.

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,6 @@
 var d3 = require('d3');
 
-var START_DATE = (new Date('2013-01-01')).getTime();
+var START_DATE = (new Date('2013-01-01'));
 var GITHUB_URL = 'https://api.github.com/users/af/repos?per_page=60';
 
 // FIXME: need CORS-enabeled proxy for Delicious
@@ -15,21 +15,36 @@ function timestamp(dateString) {
 // scale graph to browser width
 // time axes
 var githubGraph = function(config) {
+    var svg = config.el;
+    var x = d3.time.scale().range([0, config.width])
+                           .domain([START_DATE, new Date()]);
+
     var myRepos = config.data.filter(function(r) { return !r.fork })
                              .sort(function(r1, r2) { return timestamp(r1.created_at) - timestamp(r2.created_at); });
-    var all = config.el.selectAll('g.repo').data(myRepos);
+
+    var all = svg.selectAll('g.repo').data(myRepos);
     var enter = all.enter().append('g').attr('class', 'repo');
     enter.append('title')
             .text(function(d) { return d.name });
     var links = enter.append('a')
             .attr('xlink:href', function(d) { return d.html_url });
-    links.append('circle')
-            .attr('cx', function(d) {
-                return Math.floor((timestamp(d.pushed_at) - START_DATE)/(3600*1000*24));
+
+    // Draw "comet" shape for each repo:
+    links.append('path')
+            .attr('d', function(d) {
+                var width = x(new Date(d.pushed_at)) - x(new Date(d.created_at));
+                var height = 10 + Math.floor(Math.sqrt(d.stargazers_count));
+                var path = 'M0 2 ';
+                path += ('Q' + width + ' 0 ' + (width - 10) + ' ' + height + ' ');
+                path += ('L' + width + ' 0 ');
+                path += ('L' + (width - 10) + ' ' + (-height) + ' ');
+                path += ('Q' + width + ' 0 0 -2');
+                return path;
             })
-            .attr('cy', function(d, i) { return 30 + i*10 })
-            .attr('r', function(d) { return 10 + Math.floor(Math.sqrt(d.stargazers_count)); })
-            .attr('class', function(d) { return d.language });
+            .attr('transform', function(d, i) {
+                var xVal = Math.floor(x(new Date(d.created_at)));
+                return "translate(" + xVal + "," + (30 + i*20) + ")";
+            });
 };
 
 module.exports = function() {
@@ -37,6 +52,7 @@ module.exports = function() {
         if (err) return alert('gh fail');
         githubGraph({
             data: data,
+            width: window.innerWidth - 20,
             el: d3.select('.code_graph')
         });
     });
