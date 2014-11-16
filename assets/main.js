59a296e99e68c063ccb29b29005c93392f188787
Aaron Franks <aaron.franks@gmail.com>
2014-11-16 12:50:54 -0800

    WIP.

--- assets/main.js
+++ assets/main.js
@@ -89,10 +89,10 @@ module.exports = function(config) {
     var all = config.el.selectAll('g.repo').data(config.data);
     var enter = all.enter().append('g')
                     .attr('class', 'repo')
-                    .attr('transform', 'translate(-2000,20)');  // clears space for x-axis at top
+                    .attr('transform', 'translate(0,2000)');
     enter.transition()
             .delay(function(d, i) { return i*100 })
-            .duration(1000).attr('transform', 'translate(0,20)');
+            .duration(1000).attr('transform', 'translate(0,20)'); // 20px clears space for x-axis at top
 
 
     var links = enter.append('a')
