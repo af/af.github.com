1ca578c792496d297e33fd40866d5896e563f0ed
Aaron Franks <aaron.franks@gmail.com>
2016-12-15 20:18:53 -0800

    Use webpack for css, via stylepack

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import homepage from './homepage'
+import '../styles/main.styl'
 
 // Quick and dirty "router":
 if (location.pathname === '/') homepage()
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-module.exports = {
+const stylepack = require('stylepack')
+const isDev = (process.env.NODE_ENV !== 'production')
+
+const config = {
     entry: './js/main.js',
     output: {
         filename: 'assets/main.js'
     },
-    module: {
-        loaders: []
-    }
+    module: {}
 }
+
+module.exports = stylepack({
+    cssModules: false,
+    extractTo: !isDev && 'assets/main.css'
+})(config)
