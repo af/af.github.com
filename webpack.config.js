7bc1bc3f1be1d6eaabb4a0b1e2f2511d06812335
Aaron Franks <aaron.franks@gmail.com>
2017-08-15 20:15:57 -0700

    Add husky prepush script, bump stylus deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const stylepack = require('stylepack')
 const isDev = (process.env.NODE_ENV !== 'production')
 
@@ -15,10 +16,10 @@ const config = {
 }
 
 // Unfortunately can't enable uglify because it doesn't support ES6 yet :(
-// const webpack = require('webpack')
 // if (!isDev) config.plugins.push(new webpack.optimize.UglifyJsPlugin())
 
 module.exports = stylepack({
+    webpack,
     cssModules: false,
     extractTo: !isDev && 'assets/main.css'
 })(config)
