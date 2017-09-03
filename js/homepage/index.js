4a26ab22a2578b4ece3295efe1aca6843cd8262b
Aaron Franks <aaron.franks@gmail.com>
2017-09-02 21:10:09 -0700

    Reorganize homepage js and don't render timeline on mobile

--- js/homepage/index.js
+++ js/homepage/index.js
@@ -6,9 +6,49 @@ const DAYS_OF_HISTORY = 600
 const START_DATE = new Date(new Date() - DAYS_OF_HISTORY * 24 * 3600 * 1000)
 const GITHUB_URL = 'https://api.github.com/users/af/repos?sort=updated&per_page=20'
 
-const homepage = function() {
-    const svg = document.querySelector('.timelineChart')
-    const {width, height} = getComputedStyle(svg)
+
+const renderRepos = repos => {
+    const containers = document.querySelectorAll('.ossProjects > li')
+    const NUM_REPOS_TO_SHOW = containers.length
+
+    const myRepos = repos.filter(r => !r.fork)
+                      .filter(r => r.stargazers_count > 2)
+                      .sort((r1, r2) => (r1.pushed_at < r2.pushed_at ? 1 : -1))
+                      .slice(0, NUM_REPOS_TO_SHOW)
+
+    const fill = (root, selector, text) => root.querySelector(selector).textContent = text
+    containers.forEach((el, idx) => {
+        const {name, description, stargazers_count, html_url} = myRepos[idx] || {}
+        el.querySelector('a').href = html_url  // eslint-disable-line camelcase
+        fill(el, '.projectTitle', name)
+        fill(el, '.projectDesc', description)
+        fill(el, '.projectStars', stargazers_count)
+    })
+}
+
+const renderLinkSidebar = links => {
+    const container = document.querySelector('.latestLinks')
+    const htmlItems = links.slice(0, 10).map(l => `
+        <article>
+            <h1><a href="${l.u}">${l.d}</a></h1>
+            <blockquote>${l.n}</blockquote>
+            <ul class="tags">
+                ${l.t.map(t => `
+                <li>
+                    <a class="tag-${t}" href="https://pinboard.in/u:_af/t:${t}">${t}</a>
+                </li>
+                `).join('')}
+            </ul>
+            <time>${l.dt.split('T')[0]}</time>
+        </article>
+    `)
+    container.innerHTML = htmlItems.join('')
+}
+
+const renderTimeline = svg => {
+    const {width, height, display} = getComputedStyle(svg)
+    if (display === 'none') return  // Don't do expensive rendering on mobile (svg is hidden)
+
     const [svgWidth, svgHeight] = [parseInt(width), parseInt(height)]
     const margin = {top: 40, right: 0, left: 0, bottom: 60}
     const leavePadding = `translate(${svgWidth / 2}, ${margin.top})`
@@ -37,26 +77,6 @@ const homepage = function() {
         title: p.title
     }))
 
-    // Render latest links in the sidebar
-    window._linksPromise.then(links => {
-        const container = document.querySelector('.latestLinks')
-        const htmlItems = links.slice(0, 10).map(l => `
-            <article>
-                <h1><a href="${l.u}">${l.d}</a></h1>
-                <blockquote>${l.n}</blockquote>
-                <ul class="tags">
-                    ${l.t.map(t => `
-                    <li>
-                        <a class="tag-${t}" href="https://pinboard.in/u:_af/t:${t}">${t}</a>
-                    </li>
-                    `).join('')}
-                </ul>
-                <time>${l.dt.split('T')[0]}</time>
-            </article>
-        `)
-        container.innerHTML = htmlItems.join('')
-    })
-
     // Plot saved links from pinboard's JSONP API
     window._linksPromise.then(links => {
         // Divide links into tag group "buckets":
@@ -82,27 +102,21 @@ const homepage = function() {
             rootEl: select('.bubbleRoot').append('g').attr('transform', leavePadding)
         })
     })
+}
+
+
+const homepageMain = () => {
+    // Render latest links from pinboard
+    window._linksPromise.then(renderLinkSidebar)
 
     // Render Github source repos, using their CORS-enabled public API
     json(GITHUB_URL, (err, repos) => {
-        if (err) return console.error(err)
-        const containers = document.querySelectorAll('.ossProjects > li')
-        const NUM_REPOS_TO_SHOW = containers.length
-
-        const myRepos = repos.filter(r => !r.fork)
-                          .filter(r => r.stargazers_count > 2)
-                          .sort((r1, r2) => (r1.pushed_at < r2.pushed_at ? 1 : -1))
-                          .slice(0, NUM_REPOS_TO_SHOW)
-
-        const fill = (root, selector, text) => root.querySelector(selector).textContent = text
-        containers.forEach((el, idx) => {
-            const {name, description, stargazers_count, html_url} = myRepos[idx] || {}
-            el.querySelector('a').href = html_url  // eslint-disable-line camelcase
-            fill(el, '.projectTitle', name)
-            fill(el, '.projectDesc', description)
-            fill(el, '.projectStars', stargazers_count)
-        })
+        err
+            ? console.error(err)
+            : renderRepos(repos)
     })
+
+    renderTimeline(document.querySelector('.timelineChart'))
 }
 
-export default homepage
+export default homepageMain
