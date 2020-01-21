62daab45bda0e9e75a0f0e31aac56fe770757da0
Aaron Franks <aaron.franks@gmail.com>
2020-01-20 20:01:37 -0800

    WIP replacing jekyll seo tag (posts still broken)

---
+++ _data/site.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Aaron Franks',
+    description: 'Area man rambles about programming',
+    url: 'https://aaronfranks.com',
+    baseUrl: '/',
+    author: 'Aaron Franks',
+    buildTime: new Date()
+}
